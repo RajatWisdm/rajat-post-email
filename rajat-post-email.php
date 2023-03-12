da602b37c5487912aee430514c9dcb35bc9e62a5
RajatWisdm <rajat.ganguly@wisdmlabs.com>
2023-03-12 17:19:57 +0530

    Created class, made admin page and defined few functions

--- rajat-post-email.php
+++ rajat-post-email.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Plugin Name:       Rajat Plugin
+ * Plugin Name:       Post Email
  * Plugin URI:        https://github.com/plugins/the-basics/
  * Description:       This is a simple wordpress plugin that will help users schedule their post in advance
  * Version:           1.0.0
@@ -11,4 +11,50 @@
  * Text Domain:       my-basics-plugin
  * Domain Path:       /languages
  */
+
+class PostEmail{
+    public $plugin;
+
+    function __construct() {
+        $this->plugin = plugin_basename( __FILE__ );
+    }
+    
+    function register() {
+        // ADDING ADMIN PAGE
+        add_action( 'admin_menu', [ $this, 'add_admin_pages' ] );
+
+        // add_action( 'draft_to_publish', [ $this, 'send_email' ] );
+        add_action( 'publish_post', [ $this, 'send_email' ] );
+    }
+
+    function send_email( $post_id ) {
+        $post = get_post( $post_id );
+        $subject = 'New Post: ' . $post->post_title;
+        $message = 'A new post has been published on your website. View it here: ' . get_permalink( $post_id );
+        $recipient = 'dotecodegroup@gmail.com';
+        $from = "wisdm@shilavillaresort.com";
+        $header = "From: $from";
+        if(mail( $recipient, $subject, $message, $header )){
+            echo 'done';
+        }
+        else{
+            echo 'error';
+        }
+    }
+
+    function add_admin_pages() {
+        add_menu_page( 'Post Email', 'Post Email', 'manage_options', 'post_email', array( $this, 'admin_page_markup' ), 'dashicons-email-alt', 100 );
+    }
+
+    function admin_page_markup() {
+        echo "HI";
+    }
+}
+
+if ( class_exists( 'PostEmail' ) ){
+    $cal = new PostEmail();
+    $cal->register();
+ }
+
+
 ?>
