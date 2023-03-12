6ad9e1058a63f52094542fc5520f74cf3d7c5ff7
RajatWisdm <rajat.ganguly@wisdmlabs.com>
2023-03-12 11:20:59 +0530

    Project initialized

---
+++ rajat-post-email.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name:       Rajat Plugin
+ * Plugin URI:        https://github.com/plugins/the-basics/
+ * Description:       This is a simple wordpress plugin that will help users schedule their post in advance
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Rajat Ganguly
+ * Author URI:        https://github.com/RajatWisdm/
+ * Text Domain:       my-basics-plugin
+ * Domain Path:       /languages
+ */
+?>
